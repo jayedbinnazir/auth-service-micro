73fbe21c29bf8fbeb603d2620669ea21ebce4668
jayedbinnazir <jayed.freelance@gmail.com>
2024-05-12 10:16:09 +0600

    setup TypeORM

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,18 @@
 import { config } from "dotenv";
+import path from "node:path";
 
-config();
+// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+config({ path: path.join(__dirname + `../../.env.${process.env.NODE_ENV}`) });
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DB_HOST, DB_NAME, DB_PORT, DB_USERNAME, DB_PASSWORD } =
+    process.env;
 
 export const Config = {
     PORT,
     NODE_ENV,
+    DB_HOST,
+    DB_NAME,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
 };
